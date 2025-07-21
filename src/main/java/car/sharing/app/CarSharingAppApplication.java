b10b0799f38eb726c93a580e5f8db1ed3e0d69b4
Yaroslav Pryshchepa <yrikpa200022@gmail.com>
2025-07-21 19:19:16 +0300

    Added maven checkstyle plugin, Configured CI process, Added liquibase support, Added Docker and docker-compose support

--- src/main/java/car/sharing/app/CarSharingAppApplication.java
+++ src/main/java/car/sharing/app/CarSharingAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CarSharingAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CarSharingAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CarSharingAppApplication.class, args);
+    }
 
 }
