10031452b25dc2e6dae0d9fa0e5ea3f5fd4b3491
Yaroslav Pryshchepa <yrikpa200022@gmail.com>
2025-07-31 15:00:42 +0300

    added small fixes to sql files

--- src/test/resources/database/insert-cars.sql
+++ src/test/resources/database/insert-cars.sql
@@ -1,5 +1,5 @@
 INSERT INTO cars (id, model, brand, type_name, inventory, daily_fee, is_deleted)
 VALUES
-    (1, 'Model S', 'Tesla', 'SEDAN', 5, 100.00, 0),
-    (2, 'Civic', 'Honda', 'SEDAN', 3, 40.00, 0),
-    (3, 'Rav4', 'Toyota', 'SEDAN', 4, 60.00, 0);
+    (1, 'Model S', 'Tesla', 'SEDAN', 5, 100.00, false),
+    (2, 'Civic', 'Honda', 'SEDAN', 3, 40.00, false),
+    (3, 'Rav4', 'Toyota', 'SEDAN', 4, 60.00, false);
--- src/test/resources/database/insert-payments.sql
+++ src/test/resources/database/insert-payments.sql
@@ -1,5 +1,5 @@
 INSERT INTO payments (id, status, payment_type, rental_id, session_url, session_id, amount_to_pay, is_deleted)
 VALUES
-    (1, 'PAID', 'PAYMENT', 1, 'http://example.com/success1', 'sess_1', 400.00, 0),
-    (2, 'PENDING', 'PAYMENT', 2, 'http://example.com/success2', 'sess_2', 200.00, 0),
-    (3, 'PAID', 'FINE', 2, 'http://example.com/success3', 'sess_3', 50.00, 0);
+    (1, 'PAID', 'PAYMENT', 1, 'http://example.com/success1', 'sess_1', 400.00, false),
+    (2, 'PENDING', 'PAYMENT', 2, 'http://example.com/success2', 'sess_2', 200.00, false),
+    (3, 'PAID', 'FINE', 2, 'http://example.com/success3', 'sess_3', 50.00, false);
